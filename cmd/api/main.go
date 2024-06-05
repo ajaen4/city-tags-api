0e52cdef99c4b0ad9643e6dff495129e271099cc
ajaen4 <a.jaenrev@gmail.com>
2024-06-05 17:58:11 +0200

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"city-tags-api/internal/server"
+	"fmt"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(fmt.Sprintf("cannot start server: %s", err))
+	}
+}

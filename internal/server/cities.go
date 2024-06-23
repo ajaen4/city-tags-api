9babac93812e2f4b7745b6cc256ddfbdefd3ec9c
ajaen4 <a.jaenrev@gmail.com>
2024-06-23 18:26:43 +0200

    Add unit tests

--- internal/server/cities.go
+++ internal/server/cities.go
@@ -2,13 +2,74 @@ package server
 
 import (
 	"city-tags-api/internal/api_errors"
+	"fmt"
 	"net/http"
 	"strconv"
 )
 
+type getCityResp struct {
+	CityId       int    `json:"city_id"`
+	CityName     string `json:"city_name"`
+	Continent    string `json:"continent"`
+	Country3Code string `json:"country_3_code"`
+}
+
 type getCitiesResp struct {
 	Cities []getCityResp `json:"cities"`
 	Offset int           `json:"offset"`
+}
+
+type getCitiesReq struct {
+	offset int
+	limit  int
+}
+
+func (getCitR *getCitiesReq) validate(r *http.Request) error {
+	var err error
+	var offset int
+	clientErr := &api_errors.ClientErr{
+		HttpCode: http.StatusBadRequest,
+		Message:  "Parameters not present or invalid",
+	}
+
+	offsetParam := r.URL.Query().Get("offset")
+	if offsetParam == "" {
+		offset = 0
+	} else {
+		offset, err = strconv.Atoi(offsetParam)
+	}
+	if err != nil {
+		clientErr.Errors = map[string]string{
+			"offset": "Not present or invalid",
+		}
+		clientErr.LogMess = err.Error()
+	}
+
+	var limit int
+	limitParam := r.URL.Query().Get("limit")
+	if limitParam == "" {
+		limit = 100
+	} else {
+		limit, err = strconv.Atoi(limitParam)
+	}
+	if err != nil {
+		clientErr.Errors = map[string]string{
+			"offset": "Not present or invalid",
+		}
+		if clientErr.LogMess != "" {
+			clientErr.LogMess += fmt.Sprintf(", %s", err.Error())
+		} else {
+			clientErr.LogMess = err.Error()
+		}
+	}
+
+	if clientErr.LogMess != "" {
+		return clientErr
+	}
+
+	getCitR.offset = offset
+	getCitR.limit = limit
+	return nil
 }
 
 // @Summary		Get cities
@@ -19,28 +80,22 @@ type getCitiesResp struct {
 // @Param       limit   query int	false	"Limit for pagination"
 // @Success		200 	{object} 	getCityResp
 // @Failure     500 	{object} 	api_errors.ClientErr
-// @Router		/v0/cities/ [get]
+// @Router		/v0/cities [get]
 func (api *Api) getCities(w http.ResponseWriter, r *http.Request) error {
-	offsetParam := r.URL.Query().Get("offset")
-	offset, err := strconv.Atoi(offsetParam)
-	if offsetParam == "" || err != nil {
-		offset = 0
+	citiesReq := &getCitiesReq{}
+	err := citiesReq.validate(r)
+	if err != nil {
+		return err
 	}
 
-	limitParam := r.URL.Query().Get("limit")
-	limit, err := strconv.Atoi(limitParam)
-	if limitParam == "" || err != nil {
-		limit = 100
-	}
-
-	rows, err := api.db.Query("select * from city_tags.cities LIMIT $1 OFFSET $2", limit, offset)
+	rows, err := api.db.Query("select * from city_tags.cities LIMIT $1 OFFSET $2", citiesReq.limit, citiesReq.offset)
 	if err != nil {
 		return err
 	}
 
 	var citiesData = getCitiesResp{
 		Cities: []getCityResp{},
-		Offset: offset + limit,
+		Offset: citiesReq.offset + citiesReq.limit,
 	}
 	for rows.Next() {
 		cityData := getCityResp{}
@@ -55,11 +110,24 @@ func (api *Api) getCities(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-type getCityResp struct {
-	CityId       int    `json:"city_id"`
-	CityName     string `json:"city_name"`
-	Continent    string `json:"continent"`
-	Country3Code string `json:"country_3_code"`
+type getCityReq struct {
+	cityId int
+}
+
+func (getCitR *getCityReq) validate(r *http.Request) error {
+	cityIdParam := r.PathValue("cityId")
+	cityId, err := strconv.Atoi(cityIdParam)
+	if cityIdParam == "" || err != nil {
+		return &api_errors.ClientErr{
+			HttpCode: http.StatusBadRequest,
+			Message:  "Parameters not present or invalid",
+			Errors: map[string]string{
+				"cityId": "Not present or invalid",
+			},
+		}
+	}
+	getCitR.cityId = cityId
+	return nil
 }
 
 // @Summary		Get city by city id
@@ -70,16 +138,13 @@ type getCityResp struct {
 // @Failure      500  {object} api_errors.ClientErr
 // @Router			/v0/cities/{cityId} [get]
 func (api *Api) getCity(w http.ResponseWriter, r *http.Request) error {
-	cityIdParam := r.PathValue("cityId")
-	cityId, err := strconv.Atoi(cityIdParam)
-	if cityIdParam == "" || err != nil {
-		return &api_errors.ClientErr{
-			HttpCode: http.StatusBadRequest,
-			Message:  "cityId parameter not present or invalid",
-		}
+	getCityReq := getCityReq{}
+	err := getCityReq.validate(r)
+	if err != nil {
+		return err
 	}
 
-	rows, err := api.db.Query("select * from city_tags.cities where city_id = $1", cityId)
+	rows, err := api.db.Query("select * from city_tags.cities where city_id = $1", getCityReq.cityId)
 	if err != nil {
 		return err
 	}

de259593fc535484a939b9a102262c136db44f53
ajaen4 <a.jaenrev@gmail.com>
2024-07-10 11:35:19 +0200

    Refactor code to be able to run integration tests in cicd

--- docs/docs.go
+++ docs/docs.go
@@ -140,7 +140,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.0.3",
+	Version:          "0.0.4",
 	Host:             "city-tags-api.com",
 	BasePath:         "/api/v0",
 	Schemes:          []string{},

4fa776be80ca48dc07b25fcb8725a2fdaf36b272
ajaen4 <a.jaenrev@gmail.com>
2024-09-06 16:43:16 +0200

    Add a domain mapping to cloud run

--- iac/internal/containers/repository.go
+++ iac/internal/containers/repository.go
@@ -7,12 +7,12 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func NewRepository(ctx *pulumi.Context, name string) *artifactregistry.Repository {
+func NewRepository(ctx *pulumi.Context, name string, region string) *artifactregistry.Repository {
 	repository, err := artifactregistry.NewRepository(
 		ctx,
 		name,
 		&artifactregistry.RepositoryArgs{
-			Location:     pulumi.String("europe-west1"),
+			Location:     pulumi.String(region),
 			RepositoryId: pulumi.String(name),
 			Description:  pulumi.String("test repos"),
 			Format:       pulumi.String("DOCKER"),
